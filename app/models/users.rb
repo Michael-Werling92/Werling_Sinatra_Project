62fa1a6334bd7d3aa3e3ad1e5893d0fa1f6f2c0e
Michael <mfw6mw@virginia.edu>
2021-04-17 19:42:55 -0400

    added Users class including relationship, and validation.

---
+++ app/models/users.rb
@@ -0,0 +1,9 @@
+class Users < ActiveRecord::Base
+    has_secure_password
+
+    has_many :inventions
+
+    validates :username, :name, presence: true
+
+    validates :username, uniqueness :true
+end
