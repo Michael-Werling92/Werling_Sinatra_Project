87082684243656e3df4623196ba5abd520cba03c
Michael <mfw6mw@virginia.edu>
2021-04-17 20:16:08 -0400

    create user/index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
 
     get "/users" do
-        @users = User.all
+        @users = Users.all
         erb :"users/index"
     end
 
+    get "/signup" do
+        erb :"users/signup"
+    end
 end
