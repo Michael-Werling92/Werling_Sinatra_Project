6c43bd6447dedf0cc41c05240fe0e07092927028
Michael <mfw6mw@virginia.edu>
2021-04-17 20:25:46 -0400

    added post to userscontroller.  redirect pending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,14 @@ class UsersController < ApplicationController
         erb :"users/signup"
     end
 
+    post "/users" do
+        @user = Users.new(params)
+        if @user && @user.save
+            session[:user_id] = @user.id
+            redirect "/inventions"
+        else
+            erb :"users/signup"
+        end
+    end
 
 end
