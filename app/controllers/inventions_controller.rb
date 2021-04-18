d3879a2f4b87e4ef91ec24f1dec4d59b962ff68e
Michael <mfw6mw@virginia.edu>
2021-04-17 20:33:08 -0400

    added /inventions route to InventionsController

--- app/controllers/inventions_controller.rb
+++ app/controllers/inventions_controller.rb
@@ -1,3 +1,11 @@
 class InventionsController < ApplicationController
 
+    get "/inventions" do
+        if logged_in?
+            @invention = Inventions.all
+            erb :"inventions/index"
+        else
+            redirect "/login"
+        end
+    end    
 end
